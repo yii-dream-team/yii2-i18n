9684222a57c9c445073f7ff3095d302c20983aea
ValentineK <rlng-krsk@yandex.ru>
2014-11-06 15:51:53 +0800

    SourceMessageSearch model

---
+++ src/models/SourceMessageSearch.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace yiidreamteam\i18n\models;
+
+use yii\data\ActiveDataProvider;
+use Yii;
+use yii\helpers\ArrayHelper;
+use yiidreamteam\i18n\Module;
+
+class SourceMessageSearch extends SourceMessage
+{
+    const STATUS_TRANSLATED = 1;
+    const STATUS_NOT_TRANSLATED = 2;
+
+    public $status;
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            ['category', 'safe'],
+            ['message', 'safe'],
+            ['status', 'safe']
+        ];
+    }
+
+    /**
+     * @param array|null $params
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = SourceMessage::find();
+        $dataProvider = new ActiveDataProvider(['query' => $query]);
+
+        if (!($this->load($params) && $this->validate())) {
+            return $dataProvider;
+        }
+
+        if ($this->status == static::STATUS_TRANSLATED) {
+            $query->translated();
+        }
+        if ($this->status == static::STATUS_NOT_TRANSLATED) {
+            $query->notTranslated();
+        }
+
+        $query
+            ->andFilterWhere(['like', 'category', $this->category])
+            ->andFilterWhere(['like', 'message', $this->message]);
+        return $dataProvider;
+    }
+
+    public static function getStatus($id = null)
+    {
+        $statuses = [
+            self::STATUS_TRANSLATED => Module::t('Translated'),
+            self::STATUS_NOT_TRANSLATED => Module::t('Not translated'),
+        ];
+        if ($id !== null) {
+            return ArrayHelper::getValue($statuses, $id, null);
+        }
+        return $statuses;
+    }
+}
