b404c096956f8c51d19041370377fc9d8293224c
ValentineK <rlng-krsk@yandex.ru>
2014-11-06 15:34:53 +0800

    SourceMessage table model

---
+++ src/models/SourceMessage.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace yiidreamteam\i18n\models;
+
+use yii\base\InvalidConfigException;
+use Yii;
+use yii\db\ActiveRecord;
+use yiidreamteam\i18n\models\SourceMessageQuery;
+use yiidreamteam\i18n\Module;
+
+class SourceMessage extends ActiveRecord
+{
+    /**
+     * @return string
+     * @throws InvalidConfigException
+     */
+    public static function tableName()
+    {
+        $i18n = Yii::$app->getI18n();
+        if (!isset($i18n->sourceMessageTable)) {
+            throw new InvalidConfigException('You should configure i18n component');
+        }
+        return $i18n->sourceMessageTable;
+    }
+
+    public static function find()
+    {
+        return new SourceMessageQuery(static::className());
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            ['message', 'string']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Module::t('ID'),
+            'category' => Module::t('Category'),
+            'message' => Module::t('Message'),
+            'status' => Module::t('Translation status')
+        ];
+    }
+
+    public function getMessages()
+    {
+        return $this->hasMany(Message::className(), ['sourceMessageId' => 'id'])->indexBy('language');
+    }
+
+    /**
+     * @return array|SourceMessage[]
+     */
+    public static function getCategories()
+    {
+        return SourceMessage::find()->select('category')->distinct('category')->asArray()->all();
+    }
+
+    public function initMessages()
+    {
+        $messages = [];
+        foreach (Yii::$app->getI18n()->languages as $language) {
+            if (!isset($this->messages[$language])) {
+                $message = new Message;
+                $message->language = $language;
+                $messages[$language] = $message;
+            } else {
+                $messages[$language] = $this->messages[$language];
+            }
+        }
+        $this->populateRelation('messages', $messages);
+    }
+
+    public function saveMessages()
+    {
+        /** @var Message $message */
+        foreach ($this->messages as $message) {
+            $this->link('messages', $message);
+            $message->save();
+        }
+    }
+}
