e3027a9b201b123ac0ea7a164e13deb9cdde47f7
ValentineK <rlng-krsk@yandex.ru>
2014-11-06 15:49:56 +0800

    SourceMessageQuery

---
+++ src/models/SourceMessageQuery.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace yiidreamtema\i18n\models;
+
+use Yii;
+use yii\db\ActiveQuery;
+use yiidreamtema\i18n\models\Message;
+
+class SourceMessageQuery extends ActiveQuery
+{
+    public function notTranslated()
+    {
+        $messageTableName = Message::tableName();
+        $query = Message::find()->select($messageTableName . '.sourceMessageId');
+        $i = 0;
+        foreach (Yii::$app->getI18n()->languages as $language) {
+            if ($i === 0) {
+                $query->andWhere($messageTableName . '.language = :language and ' . $messageTableName . '.translation is not null', [':language' => $language]);
+            } else {
+                $query->innerJoin($messageTableName . ' t' . $i
+                    , 't' . $i . '.sourceMessageId = ' . $messageTableName . '.id and t' . $i . '.language = :language and t' . $i . '.translation is not null', [':language' => $language]);
+            }
+            $i++;
+        }
+        $ids = $query->indexBy('id')->all();
+        $this->andWhere(['not in', 'id', array_keys($ids)]);
+        return $this;
+    }
+
+    public function translated()
+    {
+        $messageTableName = Message::tableName();
+        $query = Message::find()->select($messageTableName . '.sourceMessageId');
+        $i = 0;
+        foreach (Yii::$app->getI18n()->languages as $language) {
+            if ($i === 0) {
+                $query->andWhere($messageTableName . '.language = :language and ' . $messageTableName . '.translation is not null', [':language' => $language]);
+            } else {
+                $query->innerJoin($messageTableName . ' t' . $i
+                    , 't' . $i . '.sourceMessageId = ' . $messageTableName . '.sourceMessageId and t' . $i . '.language = :language and t' . $i . '.translation is not null', [':language' => $language]);
+            }
+            $i++;
+        }
+        $ids = $query->indexBy('id')->all();
+        $this->andWhere(['in', 'id', array_keys($ids)]);
+        return $this;
+    }
+}
