6f354ce000672183623a5c20b4b55bcf3e69ae67
ValentineK <rlng-krsk@yandex.ru>
2014-11-06 15:09:18 +0800

    Message table model

---
+++ src/models/Message.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace yiidreamteam\i18n\models;
+
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\db\ActiveRecord;
+use yiidreamteam\i18n\Module;
+
+class Message extends ActiveRecord
+{
+    /**
+     * @return string
+     * @throws InvalidConfigException
+     */
+    public static function tableName()
+    {
+        $i18n = Yii::$app->getI18n();
+        if (!isset($i18n->messageTable)) {
+            throw new InvalidConfigException('You should configure i18n component');
+        }
+        return $i18n->messageTable;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['language', 'sourceMessageId'], 'required'],
+            ['sourceMessageId', 'integer'],
+            ['language', 'string', 'max' => 16],
+            ['translation', 'string']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'sourceMessageId' => Module::t('ID'),
+            'language' => Module::t('Language'),
+            'translation' => Module::t('Translation')
+        ];
+    }
+
+    public function getSourceMessage()
+    {
+        return $this->hasOne(SourceMessage::className(), ['id' => 'sourceMessageId']);
+    }
+}
